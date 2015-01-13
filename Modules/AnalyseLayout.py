bfdfad0ce92617ec4fefa92255d6e1e2c3063dd5
HaolinRen <rhlsunflower@126.com>
2015-01-13 06:55:28 +0100

    exchange some functions

--- Modules/AnalyseLayout.py
+++ Modules/AnalyseLayout.py
@@ -27,6 +27,78 @@ class AnalyseLayout(graphObject):
 			self.setNodeS(costNode, cubeSize, costHeight, Shape_cube)
 			self.addLabel(costNode, str(costPercent)+'%', Label_top)
 			x += cubeDistance
+
+	
+	def addStickGraph(self, dic, x):
+		stickX = x
+		sumsOfHeight = 5 * cubeSize
+		sumsOfDic = 0
+		for key in dic.keys():
+			sumsOfDic += dic[key]
+		#add meta node
+		for key in dic.keys():
+			tempNode = self.graph.addNode()
+			percent = dic[key] / sumsOfDic * 100 
+			hight = percent * sumsOfHeight
+			stickY = hight / 2
+			self.setNodeC(tempNode, stickX, stickY)
+			self.setNodeS(tempNode, cubeSize, hight, Shape_cubeOutlined)
+			labelInfo = str(percent)[0:4] + '%'
+			self.addLabel(tempNode,labelInfo, Label_center)
+			self.addInfo(stickX,0,key)
+			stickX += cubeDistance
+
+	def setLayoutDays(self, matrix, activityDays):
+
+		self.clearNodes()	
+		self.clearAllEdges()
+		M = len(matrix)
+		lastMonth = matrix[0][0][1]
+		x = y = 0
+		
+		def drawCube():
+			self.addInfo(400, 50, "ADM's Activity")
+			startMonth = lastMonth
+			cordX = cordY = 0
+			Aindex = 0
+			for year in range(2011, 2015):
+				for month in range(startMonth, 13):
+					monthRange = calendar.monthrange(year, month)[1]
+					for days in range(1, monthRange + 1):
+						cordX = days * cubeDistance
+						
+						tempNode = self.graph.addNode()
+						self.setNodeC(tempNode, cordX, cordY, Color_yellow, -1)
+						self.setNodeS(tempNode, cubeSize, cubeSize, Shape_cube)
+						if days == monthRange:
+							if Aindex < len(activityDays):
+								self.addLabel(tempNode, str(activityDays[Aindex]),Label_right)
+								Aindex += 1
+						
+						if cordX == cubeDistance and month == startMonth:
+							self.addLabel(tempNode, str(year) + '. '+str(month)+'  ', Label_left,Color_red)
+						elif cordX == cubeDistance:
+							self.addLabel(tempNode, str(month) + '  ', Label_left)
+					cordY -= cubeDistance
+					if month == 12:
+						startMonth = 1
+						cordY -= cubeDistance
+		drawCube()
+		for itemIndex in range(M):
+			item = matrix[itemIndex]
+			x = item[0][2] * cubeDistance
+			if item[0][1] != lastMonth:
+				y -= cubeDistance
+				if item[0][1] == 1:
+					y -= cubeDistance
+				lastMonth = item[0][1]
+			self.setNodeS(item[NODE],cubeSize,cubeSize, Shape_cube,2)
+#			self.viewLayout[item[NODE]] = tlp.Coord(x, y,2)
+			if item[TRANSACTION] == 'Acquired':
+				nodeColor = Color_red
+			else:
+				nodeColor = Color_blue
+			self.setNodeC(item[NODE],x,y,nodeColor)
 		
 
 
