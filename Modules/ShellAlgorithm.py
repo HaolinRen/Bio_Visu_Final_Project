53210cdf2e89e30c423a85ee434d5f3d822d2933
HaolinRen <rhlsunflower@126.com>
2015-01-14 13:20:22 +0100

    modify the shell algorithm.

--- Modules/ShellAlgorithm.py
+++ Modules/ShellAlgorithm.py
@@ -11,34 +11,52 @@ class ShellAlgorithm(object):
 		sedSize = sizeList[1]
 		radius = sedSize + preSize
 		sedCircle = [radius, 0, sedSize]
-		calCircle1 = [x for x in preCircle]
-		calCircle2 = [x for x in sedCircle]
+		calCircle1 = preCircle
+		calCircle2 = sedCircle
 		result.append(preCircle)
 		result.append(sedCircle)
 		self.__shellQueue.append(preCircle)
 		self.__shellQueue.append(sedCircle)
 		
 		M = len(sizeList)
+		index1 = 0
 		for sizeInfo in sizeList[2:M]:
+			index2 = 1
 			thirdCircle = self.calThirdCircleCord(calCircle1,calCircle2,sizeInfo)
 			while not self.testValidCord(thirdCircle):
-				del self.__shellQueue[0]
-				calCircle1 = [x for x in self.__shellQueue[0]]
-				calCircle2 = [x for x in self.__shellQueue[-1]]
-				thirdCircle = self.calThirdCircleCord(calCircle1,calCircle2,sizeInfo)
-				if len(self.__shellQueue) == 0:
-					return 0
-			validCircle = [x for x in thirdCircle]
+				calCircle1 = self.__shellQueue[index1]
+				if testValidTriangle(calCircle1,calCircle2,sizeInfo):
+					thirdCircle = self.calThirdCircleCord(calCircle1,calCircle2,sizeInfo)
+				if index1 < len(self.__shellQueue):
+					index1 += 1
+				if index1 == len(self.__shellQueue):
+					del self.__shellQueue[0]
+					calCircle2 = self.__shellQueue[-index2]
+					index += 1
+
 			self.__shellQueue.append(thirdCircle)
-			result.append(validCircle)
-			calCircle2 = [x for x in thirdCircle]
-			
-		return result
+			result.append(thirdCircle)
+			calCircle2 = thirdCircle
+		
+		return result	
+		
+	def testValidTriangle(self, circleST, circleED, radiusRD):
+		x1 = circleST[0]
+		y1 = circleST[1]
+		r1 = circleST[2]
+		x2 = circleED[0]
+		y2 = circleED[1]
+		r2 = circleED[2]
+		C = math.sqrt(float(x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))
+		B = r1 + radiusRD
+		A = r2 + radiusRD
+		if C > A + B:
+			return False
+		else:
+			return True
 
 	def testValidCord(self, thirdCircle):
 		for circle in self.__shellQueue:
-#			print self.__shellQueue
-#			print thirdCircle,' ,' ,circle,' : ', self.isValidCord(thirdCircle, circle)
 			if not self.isValidCord(thirdCircle, circle):
 				return False
 		return True
@@ -50,9 +68,7 @@ class ShellAlgorithm(object):
 		x2 = existCircle[0]
 		y2 = existCircle[1]
 		r2 = existCircle[2]
-		
 		dist = math.ceil(math.sqrt(float(x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)))
-		
 		if dist < r1 + r2:
 			return False
 		else:
@@ -91,8 +107,3 @@ class ShellAlgorithm(object):
 
 		return [x3, y3, radiusRD]
 		
-testList = [2,3,3,6,3,21,14,34,3,4]
-
-myShell = ShellAlgorithm()
-print myShell.getShellCord(testList)
-#print myShell.calThirdCircleCord([5,0,3],[-7.418,2.997,6],88)
