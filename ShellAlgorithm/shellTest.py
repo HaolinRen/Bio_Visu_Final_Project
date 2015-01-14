0daaffecf042481a349e73373480076915d827b2
HaolinRen <rhlsunflower@126.com>
2015-01-14 12:57:21 +0100

    finished the shell algorithm

---
+++ ShellAlgorithm/shellTest.py
@@ -0,0 +1,88 @@
+# Powered by Python 2.7
+
+# To cancel the modifications performed by the script
+# on the current graph, click on the undo button.
+
+# Some useful keyboards shortcuts : 
+#   * Ctrl + D : comment selected lines.
+#   * Ctrl + Shift + D  : uncomment selected lines.
+#   * Ctrl + I : indent selected lines.
+#   * Ctrl + Shift + I  : unindent selected lines.
+#   * Ctrl + Return  : run script.
+#   * Ctrl + F  : find selected text.
+#   * Ctrl + R  : replace selected text.
+#   * Ctrl + Space  : show auto-completion dialog.
+
+from shell import ShellAlgorithm
+from tulip import *
+from time import *
+import random
+
+# the updateVisualization(centerViews = True) function can be called
+# during script execution to update the opened views
+
+# the pauseScript() function can be called to pause the script execution.
+# To resume the script execution, you will have to click on the "Run script " button.
+
+# the runGraphScript(scriptFile, graph) function can be called to launch another edited script on a tlp.Graph object.
+# The scriptFile parameter defines the script name to call (in the form [a-zA-Z0-9_]+.py)
+
+# the main(graph) function must be defined 
+# to run the script on the current graph
+
+def main(graph): 
+	viewBorderColor =  graph.getColorProperty("viewBorderColor")
+	viewBorderWidth =  graph.getDoubleProperty("viewBorderWidth")
+	viewColor =  graph.getColorProperty("viewColor")
+	viewFont =  graph.getStringProperty("viewFont")
+	viewFontSize =  graph.getIntegerProperty("viewFontSize")
+	viewLabel =  graph.getStringProperty("viewLabel")
+	viewLabelBorderColor =  graph.getColorProperty("viewLabelBorderColor")
+	viewLabelBorderWidth =  graph.getDoubleProperty("viewLabelBorderWidth")
+	viewLabelColor =  graph.getColorProperty("viewLabelColor")
+	viewLabelPosition =  graph.getIntegerProperty("viewLabelPosition")
+	viewLayout =  graph.getLayoutProperty("viewLayout")
+	viewMetaGraph =  graph.getGraphProperty("viewMetaGraph")
+	viewMetric =  graph.getDoubleProperty("viewMetric")
+	viewRotation =  graph.getDoubleProperty("viewRotation")
+	viewSelection =  graph.getBooleanProperty("viewSelection")
+	viewShape =  graph.getIntegerProperty("viewShape")
+	viewSize =  graph.getSizeProperty("viewSize")
+	viewSrcAnchorShape =  graph.getIntegerProperty("viewSrcAnchorShape")
+	viewSrcAnchorSize =  graph.getSizeProperty("viewSrcAnchorSize")
+	viewTexture =  graph.getStringProperty("viewTexture")
+	viewTgtAnchorShape =  graph.getIntegerProperty("viewTgtAnchorShape")
+	viewTgtAnchorSize =  graph.getSizeProperty("viewTgtAnchorSize")
+
+	testList = []
+	for i in range(10):
+		testList.append(random.randint(5,20))
+
+	
+	myShell = ShellAlgorithm()
+	res = myShell.getShellCord(testList)
+	index = 0
+	
+	graph.delNodes(graph.getNodes())
+	for i in range(len(testList)):
+		c1 = random.randint(200,225)
+		c2 = random.randint(200,225)
+		c3 = random.randint(200,225)
+		tempNode = graph.addNode()	
+		viewColor[tempNode] = tlp.Color(c1,c2,c3)
+	
+	for node in graph.getNodes():
+		if index == len(res):
+			break
+		x = res[index][0]
+		y = res[index][1]
+		size = res[index][2] * 2
+		viewSize[node] = tlp.Size(size,size,1)
+		viewShape[node] = tlp.NodeShape.Circle
+		viewLayout[node] = tlp.Coord(x,y,1)
+		sleep(0.05)
+		updateVisualization()
+		index += 1	
+	
+	
+		
